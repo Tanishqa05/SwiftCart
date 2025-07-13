684f0f113ceb63f29d2169a1bd9c924accab4058
Tanishqa05 <tani05bandi@gmail.com>
2025-07-14 00:50:00 +0530

    connected to mongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,18 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import { connectDB } from './config/db.js';
+
+dotenv.config();
 
 const app = express();
 
-app.get('/', (req, res) => {
+app.get('/products', (req, res) => {
     res.send('Hello World!');
 })
 
+console.log(process.env.MONGO_URI);
+
 app.listen(5000, () => {
+    connectDB();
     console.log('Server is running at http://localhost:5000');
 })
