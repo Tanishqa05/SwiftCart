13b0c0c1c6f04f966b820a4f3fd48fe21108a53d
Tanishqa05 <tani05bandi@gmail.com>
2025-07-14 23:52:58 +0530

    created the product schema and reviewed the post and delete method for the products in postman and mongodb atlas

--- backend/models/product.model.js
+++ backend/models/product.model.js
@@ -1,1 +1,23 @@
 import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Please add a product name'],
+    },
+    price: {
+        type: Number,
+        required: [true, 'Please add a product price'],
+    },
+    image: {
+        type: String,
+        required: [true, 'Please add a product image'],
+    },
+}, {
+    timestamps: true,
+});
+
+const Product = mongoose.model('Product', productSchema);
+
+export default Product;
+// This code defines a Mongoose schema and model for a product, which includes fields for name
--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,47 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import { connectDB } from './config/db.js';
+import Product from './models/product.model.js'; // Ensure this path is correct
 
 dotenv.config();
 
 const app = express();
 
-app.get('/products', (req, res) => {
-    res.send('Hello World!');
-})
+app.use(express.json()); //allows us to accept json data in requests, a middleware
 
-console.log(process.env.MONGO_URI);
+app.post("/api/products", async(req, res) => {
+    const product= req.body; //user will send product data in the request body
+
+    if (!product.name || !product.price || !product.image) {
+        return res.status(400).json({ success: false, message: 'PLease provide all fields' });
+    }
+
+    const newProduct= new Product(product);
+
+    try {
+        await newProduct.save();
+        res.status(201).json({ success: true, data: newProduct });
+    }
+    catch (error) {
+        console.error(`Error: ${error.message}`);
+        res.status(500).json({ success: false, message: "Server Error" });
+    }
+});
+
+app.delete("/api/products/:id", async(req, res) => {
+    const { id } = req.params;
+    try {
+        const product = await Product.findByIdAndDelete(id);
+        if (!product) {
+            return res.status(404).json({ success: false, message: 'Product not found' });
+        }
+        res.status(200).json({ success: true, message: 'Product deleted successfully' });
+    } 
+    catch (error) {
+        res.status(404).json({ success: false, message: "Product not found" });
+    }
+});
+
 
 app.listen(5000, () => {
     connectDB();
