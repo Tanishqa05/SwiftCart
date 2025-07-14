0ece5dab840c4e13c247374187554ba0caf9b7a6
Tanishqa05 <tani05bandi@gmail.com>
2025-07-15 00:29:30 +0530

    created and tested, get and update api, through postman and atlas

--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,24 @@ import express from 'express';
 import dotenv from 'dotenv';
 import { connectDB } from './config/db.js';
 import Product from './models/product.model.js'; // Ensure this path is correct
+import mongoose from 'mongoose';
 
 dotenv.config();
 
 const app = express();
 
 app.use(express.json()); //allows us to accept json data in requests, a middleware
+
+app.get("/api/products", async (req, res) => {
+    try {    
+        const products = await Product.find({});
+        res.status(200).json({ success: true, data: products });
+    } 
+    catch (error) {
+        console.error(`Error: ${error.message}`);
+        res.status(500).json({ success: false, message: "Server Error" });
+    }
+});
 
 app.post("/api/products", async(req, res) => {
     const product= req.body; //user will send product data in the request body
@@ -24,6 +36,22 @@ app.post("/api/products", async(req, res) => {
     }
     catch (error) {
         console.error(`Error: ${error.message}`);
+        res.status(500).json({ success: false, message: "Server Error" });
+    }
+});
+
+app.put("/api/products/:id", async(req, res) => {
+    const { id } = req.params;
+    const product = req.body;
+
+    if(mongoose.Types.ObjectId.isValid(id) === false) {
+        return res.status(404).json({ success: false, message: 'Invalid Product ID' });}
+
+    try {
+        const updatedProduct = await Product.findByIdAndUpdate(id, product, { new: true });
+        res.status(200).json({ success: true, data: updatedProduct });
+    }
+    catch (error) {
         res.status(500).json({ success: false, message: "Server Error" });
     }
 });
